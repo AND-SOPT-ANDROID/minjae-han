eea351012befe117db64e763104c6221fda03bb1
MinJae Han <hanminjae131@gmail.com>
2024-10-10 00:16:01 +0900

    [chore]#1 TextField 높이 변경

--- app/src/main/java/org/sopt/and/SignUpActivity.kt
+++ app/src/main/java/org/sopt/and/SignUpActivity.kt
@@ -18,14 +18,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -35,10 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -107,7 +100,8 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
             value = emailText,
             onValueChange = { emailText = it },
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(60.dp),
             placeholder = {
                 Text(
                 text = "wavve@example.com",
@@ -142,7 +136,8 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
             value = passWordText,
             onValueChange = { passWordText = it },
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(60.dp),
             placeholder = {
                 Text(
                     text = "wavve 비밀번호 설정",
