ba5e0abe8426580fe19d33074cb23cd7a025e24f
MinJae Han <hanminjae131@gmail.com>
2024-10-10 02:10:30 +0900

    [feat]#1 회원가입 버튼 추가 및 레이아웃 수정

--- app/src/main/java/org/sopt/and/SignUpActivity.kt
+++ app/src/main/java/org/sopt/and/SignUpActivity.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -55,191 +56,206 @@ class SignUpActivity : ComponentActivity() {
 
 @Composable
 fun SignUpScreen(modifier: Modifier = Modifier) {
-    Column(
+    Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Black)
-            .padding(20.dp)
+            .background(color = Color.Black),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
-        //상단라인
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Spacer(modifier = Modifier)
+        Column (modifier = Modifier.padding(20.dp)) {
+            //상단라인
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Spacer(modifier = Modifier)
 
-            Text(
-                text = "회원가입",
-                color = Color.White,
-                fontSize = 20.sp,
-                modifier = Modifier.align(Alignment.CenterVertically)
-            )
-            Text(
-                color = Color.White,
-                text = "X",
-                fontSize = 30.sp,
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .clickable { }
-            )
-        }
-
-        Spacer(modifier = Modifier.height(40.dp))
-        //안내 텍스트 문구
-        Text(
-            modifier = Modifier,
-            text = "이메일과 비밀번호 만으로\nWavve를 즐길 수 있어요!",
-            fontSize = 23.sp,
-            color = Color.White,
-        )
-
-        Spacer(modifier = Modifier.height(40.dp))
-        //아이디 입력 필드
-        var emailText by remember { mutableStateOf("") }
-        TextField(
-            value = emailText,
-            onValueChange = { emailText = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(60.dp),
-            placeholder = {
                 Text(
-                text = "wavve@example.com",
-                color = Color.Gray
-                ) },
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.DarkGray,
-                unfocusedContainerColor = Color.DarkGray,
-                focusedTextColor = Color.White,
-                unfocusedTextColor = Color.White,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            ),
-            shape = RoundedCornerShape(5.dp),
-            singleLine = true
-        )
-
-        Spacer(modifier = Modifier.height(10.dp))
-        //아이디 주의사항 텍스트
-        Text(
-            text = "! 로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을\n입력해 주세요.",
-            color = Color.Gray,
-        )
-
-        Spacer(modifier = Modifier.height(20.dp))
-
-        // 비밀번호 입력 필드
-        var passWordText by remember { mutableStateOf("") }
-        var showPassword by remember { mutableStateOf(false) } // "Show"와 "Hide"를 위한 상태 저장
-
-        TextField(
-            value = passWordText,
-            onValueChange = { passWordText = it },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(60.dp),
-            placeholder = {
-                Text(
-                    text = "wavve 비밀번호 설정",
-                    color = Color.Gray
-                )
-            },
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = Color.DarkGray,
-                unfocusedContainerColor = Color.DarkGray,
-                focusedTextColor = Color.White,
-                unfocusedTextColor = Color.White,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent
-            ),
-            shape = RoundedCornerShape(5.dp),
-            visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(), // 비밀번호 보임 여부 결정
-            trailingIcon = {
-                Text(
-                    text = if (showPassword) "Hide" else "Show", // 상태에 따라 텍스트 변경
+                    text = "회원가입",
                     color = Color.White,
-                    modifier = Modifier.clickable {
-                        showPassword = !showPassword // 클릭 시 상태 변경
-                    }
+                    fontSize = 20.sp,
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
-            },
-            singleLine = true
-        )
+                Text(
+                    color = Color.White,
+                    text = "X",
+                    fontSize = 30.sp,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .clickable { }
+                )
+            }
 
-        Spacer(modifier = Modifier.height(10.dp))
-        //아이디 주의사항 텍스트
-        Text(
-            text = "! 비밀번호는 8-20자 이내, 영문 대소문자, 숫자, 특수문자 중\n 3가지 이상 혼용하여 입력해 주세요.",
-            color = Color.Gray,
-        )
+            Spacer(modifier = Modifier.height(40.dp))
+            //안내 텍스트 문구
+            Text(
+                modifier = Modifier,
+                text = "이메일과 비밀번호 만으로\nWavve를 즐길 수 있어요!",
+                fontSize = 23.sp,
+                color = Color.White,
+            )
 
-        Spacer(modifier = Modifier.height(55.dp))
-        Text(
-            text = "또는 다른 서비스 계정으로 가입",
-            color = Color.Gray,
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center
-        )
+            Spacer(modifier = Modifier.height(40.dp))
+            //아이디 입력 필드
+            var emailText by remember { mutableStateOf("") }
+            TextField(
+                value = emailText,
+                onValueChange = { emailText = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                placeholder = {
+                    Text(
+                        text = "wavve@example.com",
+                        color = Color.Gray
+                    ) },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.DarkGray,
+                    unfocusedContainerColor = Color.DarkGray,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(5.dp),
+                singleLine = true
+            )
 
-        Spacer(modifier = Modifier.height(30.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Button(
-                onClick = {},
-                shape = CircleShape,
+            Spacer(modifier = Modifier.height(10.dp))
+            //아이디 주의사항 텍스트
+            Text(
+                text = "! 로그인, 비밀번호 찾기, 알림에 사용되니 정확한 이메일을\n입력해 주세요.",
+                color = Color.Gray,
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // 비밀번호 입력 필드
+            var passWordText by remember { mutableStateOf("") }
+            var showPassword by remember { mutableStateOf(false) } // "Show"와 "Hide"를 위한 상태 저장
+
+            TextField(
+                value = passWordText,
+                onValueChange = { passWordText = it },
                 modifier = Modifier
-                    .padding(5.dp)
-                    .size(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
-            ) { }
-            Button(
-                onClick = {},
-                shape = CircleShape,
-                modifier = Modifier
-                    .padding(5.dp)
-                    .size(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
-            ) { }
-            Button(
-                onClick = {},
-                shape = CircleShape,
-                modifier = Modifier
-                    .padding(5.dp)
-                    .size(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
-            ) { }
-            Button(
-                onClick = {},
-                shape = CircleShape,
-                modifier = Modifier
-                    .padding(5.dp)
-                    .size(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF113065))
-            ) { }
-            Button(
-                onClick = {},
-                shape = CircleShape,
-                modifier = Modifier
-                    .padding(5.dp)
-                    .size(50.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.White)
-            ) { }
+                    .fillMaxWidth()
+                    .height(60.dp),
+                placeholder = {
+                    Text(
+                        text = "wavve 비밀번호 설정",
+                        color = Color.Gray
+                    )
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.DarkGray,
+                    unfocusedContainerColor = Color.DarkGray,
+                    focusedTextColor = Color.White,
+                    unfocusedTextColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(5.dp),
+                visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(), // 비밀번호 보임 여부 결정
+                trailingIcon = {
+                    Text(
+                        text = if (showPassword) "Hide" else "Show", // 상태에 따라 텍스트 변경
+                        color = Color.White,
+                        modifier = Modifier.clickable {
+                            showPassword = !showPassword // 클릭 시 상태 변경
+                        }
+                    )
+                },
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+            //아이디 주의사항 텍스트
+            Text(
+                text = "! 비밀번호는 8-20자 이내, 영문 대소문자, 숫자, 특수문자 중\n 3가지 이상 혼용하여 입력해 주세요.",
+                color = Color.Gray,
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+            Text(
+                text = "또는 다른 서비스 계정으로 가입",
+                color = Color.Gray,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {},
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow)
+                ) { }
+                Button(
+                    onClick = {},
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+                ) { }
+                Button(
+                    onClick = {},
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
+                ) { }
+                Button(
+                    onClick = {},
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF113065))
+                ) { }
+                Button(
+                    onClick = {},
+                    shape = CircleShape,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(50.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+                ) { }
+            }
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            //주의사항 텍스트
+            Text(
+                text = "• SNS계정으로 간편하게 가입하여 서비스를 이용하실 수 있습니다. 기존 POOQ 계정 또는 Wavve 계정과는 연동되지 않으니 이용에 참고하세요.",
+                color = Color.Gray,
+            )
+
         }
 
-        Spacer(modifier = Modifier.height(30.dp))
+        var isEnabled by remember { mutableStateOf(true) }
 
-        //주의사항 텍스트
-        Text(
-            text = "• SNS계정으로 간편하게 가입하여 서비스를 이용하실 수 있습니다. 기존 POOQ 계정 또는 Wavve 계정과는 연동되지 않으니 이용에 참고하세요.",
-            color = Color.Gray,
-        )
-
-
-
-
-
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = if (isEnabled) Color.Blue else Color.Gray, // 활성화/비활성화 색상 설정
+                contentColor = if (isEnabled) Color.White else Color.White, // 텍스트 색상 (활성화/비활성화 동일)
+            ),
+            shape = RectangleShape, // 사각형 모양
+            enabled = isEnabled // 버튼 활성화 여부
+        ) {
+            Text("Wavve 회원가입", fontSize = 17.sp)
+        }
     }
 }
 
